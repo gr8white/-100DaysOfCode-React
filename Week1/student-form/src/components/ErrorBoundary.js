b5a91cd58978b6a4888697468489845127bbd2e7
Derrick White <gr8white@uga.edu>
2019-04-22 10:19:17 -0500

    Day 7

---
+++ Week1/student-form/src/components/ErrorBoundary.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class ErrorBoundary extends Component {
+  state = {
+    hasError: false
+  }
+
+  componentDidCatch() {
+    this.setState({hasError: true})
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something broke bruh</h2>
+    }
+    return this.props.children
+  }
+}
