6957f529a48d14e41d63548b1282648f1c1364b2
Derrick White <gr8white@uga.edu>
2019-05-08 16:48:14 -0500

    Day 24

---
+++ Week4/todoapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import TodoItem from './TodoItem'
+import todosData from "./todosData"
+
+function App() {
+  const todoComponents = todosData.map(todo => <TodoItem key={todo.id} todo={todo}/> )
+
+  return (
+    <div className='todo-list'>
+      {todoComponents}
+    </div>
+  );
+}
+
+export default App;
