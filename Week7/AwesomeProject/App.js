2fc693ed29c028c9518c7f5037e75564904c7222
Derrick White <gr8white@uga.edu>
2019-06-05 13:35:44 -0500

    Day 43

--- Week7/AwesomeProject/App.js
+++ Week7/AwesomeProject/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      // <View style={styles.container}>
+      //   {/* <Text>Young D White and Young Syd!</Text> */}
+      //   <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+      //   <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+      //   <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      // </View>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <Button
+          onPress={() => {
+            Alert.alert('You tapped the button!');
+          }}
+          title="Press Me"
+        />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
+      
     );
   }
 }
