6c35777d89d12488d31211b873fc433d29f6bc74
Derrick White <gr8white@uga.edu>
2019-06-10 09:53:29 -0500

    Day 45

--- Week7/CurrencyConverter/app/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Home from './screens/Home'
-import EStyleSheet from 'react-native-extended-stylesheet'
-// import { StyleSheet, Text, View } from 'react-native';
-EStyleSheet.build({
-  $primaryBlue: '#4f6d7a'
-})
-
-export default () => <Home />
