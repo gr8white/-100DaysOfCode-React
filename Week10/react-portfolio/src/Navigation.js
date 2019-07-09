a1e93f49e44433d6b0da188d9bb3ee243b3c9b2c
Derrick White <gr8white@uga.edu>
2019-07-09 09:07:01 -0500

    Day 61

---
+++ Week10/react-portfolio/src/Navigation.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Navigation.css'
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav>
+        <h2 className="logo">React Project</h2>
+
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navigation
