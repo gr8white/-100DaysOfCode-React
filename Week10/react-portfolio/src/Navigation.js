f87780fdb75d0eb81e83241ab6fcc9eb528a4ed2
Derrick White <gr8white@uga.edu>
2019-07-10 22:17:23 -0500

    Day 62

--- Week10/react-portfolio/src/Navigation.js
+++ Week10/react-portfolio/src/Navigation.js
@@ -12,7 +12,7 @@ class Navigation extends Component {
           <li><a href="#">About</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">Projects</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="#">Contact (skip day)</a></li>
         </ul>
       </nav>
     )
