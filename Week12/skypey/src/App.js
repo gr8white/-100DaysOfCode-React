b18d9f0a7b918187e8d5065c23c2467dfbc01cca
Derrick White <gr8white@uga.edu>
2019-08-15 12:39:28 -0500

    Day 80

---
+++ Week12/skypey/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from './components/Sidebar.js'
+import Main from './components/Main.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
